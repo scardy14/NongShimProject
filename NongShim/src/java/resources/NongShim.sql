ab50344e19590bfa9737faefb1cfcca2e736af46
989since <jdkb0918@gmail.com>
2022-11-02 14:39:08 +0900

    nongshim member table 수정 unique 추가 바람

--- NongShim/src/java/resources/NongShim.sql
+++ NongShim/src/java/resources/NongShim.sql
@@ -1,3 +1,4 @@
+-- 1. member 회원정보 테이블 
 CREATE TABLE NongShim_Member(
 	id VARCHAR2(100) PRIMARY KEY,
 	password VARCHAR2(100) NOT NULL,
@@ -5,19 +6,20 @@ CREATE TABLE NongShim_Member(
 	address VARCHAR2(100) NOT NULL,
 	nickName VARCHAR2(100) NOT NULL,
 	tel VARCHAR2(100) NOT NULL,
-	sellerInfo VARCHAR2(100) NOT NULL,
-	adminiINfo VARCHAR2(100) NOT NULL,
+	seller_Info VARCHAR2(100) default '일반',
+	admin_INfo VARCHAR2(100) default '유저',
 	email VARCHAR2(100) NOT NULL,
 	point NUMBER DEFAULT 0,
-	accountNo VARCHAR2(100)	
+	account_No VARCHAR2(100) constraint account_uq unique(account_no)
 )
-create sequence NongShim_Member_seq;
 
-insert into NongShim_Member(id,password,name,address,nickName,tel,sellerInfo,adminiInfo,email,point,accountNo) 
-values('java','a','홍주영','길동','홍','010','판매자1','관리자','daum',0,'356');
-insert into NongShim_Member(id,password,name,address,nickName,tel,sellerInfo,adminiInfo,email,point,accountNo) 
-values('spring','a','홍주영','길동','홍','011','판매자1','관리자','daum',0,'356');
-insert into NongShim_Member(id,password,name,address,nickName,tel,sellerInfo,adminiInfo,email,point,accountNo) 
-values('del','a','홍주영','길동','홍','012','판매자1','관리자','daum',0,'356');
+--1.1 회원정보 데이터 insert
+insert into NongShim_Member(id,password,name,address,nickName,tel,seller_Info,admin_Info,email,point,account_No) 
+values('java','a','홍주영','서울','주영','010','daum',0,'356');
+insert into NongShim_Member(id,password,name,address,nickName,tel,seller_Info,admin_Info,email,point,account_No) 
+values('spring','a','장지영','판교','지영','011','daum',0,'356');
+insert into NongShim_Member(id,password,name,address,nickName,tel,seller_Info,admin_Info,email,point,account_No) 
+values('del','a','김민석','대구','민석','012','daum',0,'356');
 
+--1.2 회원정보 select
 select*from NongShim_Member;
