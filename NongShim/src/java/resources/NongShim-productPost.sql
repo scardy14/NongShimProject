6896fe3e20043cf37dc01d793a586e944d1b3ff1
989since <jdkb0918@gmail.com>
2022-11-02 15:48:42 +0900

    jdk_push : sql 주석

--- NongShim/src/java/resources/NongShim-productPost.sql
+++ NongShim/src/java/resources/NongShim-productPost.sql
@@ -1,5 +1,6 @@
 drop sequence postNo_seq
 
+-- 2. Product Post 상품 게시판 글
 create table NongShim_productPost(
 postNo number primary key,
 title varchar2(100) not null,
@@ -14,9 +15,10 @@ status varchar2(100)not null,
 CONSTRAINT fk_pp FOREIGN KEY(id) REFERENCES NongShim_Member(id)
 );
 
-
+-- 2.1 시퀀스 생성
 create sequence postNo_seq;
 
+-- 2.2 product post data 입력 
 insert into NongShim_productPost
 values (postNo_seq.nextval, '초록빛깔 상추', '신선하고 맛있습니다','java',default,'민석짱','코멘트부분',sysdate,'야채','status');
 insert into NongShim_productPost
@@ -25,6 +27,6 @@ insert into NongShim_productPost
 values (postNo_seq.nextval, '맛있는 햅쌀', '꼬들꼬들합니다','jdbc',default,'민석짱3','코멘트부분',sysdate,'곡물','status');
 
 
-
+-- 2.3 product post 
 select * from NongShim_productpost
 
