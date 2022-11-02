d6ff06c90f0343da60330648c8a12e1876d03c7e
scardy14 <heperion14@gmail.com>
2022-11-02 16:59:18 +0900

    뭔가하는중

--- NongShim/src/main/java/controller/FindPostListController.java
+++ NongShim/src/main/java/controller/FindPostListController.java
@@ -3,9 +3,8 @@ package controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.kosta.myproject.model.BoardDAO;
-import org.kosta.myproject.model.Pagination;
 
+import model.Pagination;
 import model.ProductPostDAO;
 
 public class FindPostListController implements Controller{
@@ -16,18 +15,17 @@ public class FindPostListController implements Controller{
 		
 		String pageNo=request.getParameter("pageNo");
 		Pagination pagination=null;
-		long totalPostCount=BoardDAO.getInstance().getTotalPostCount();
+		int totalPostCount=dao.getTotalPostCount();
 		if(pageNo==null) {
 			pagination=new Pagination(totalPostCount);
 		}else {
-			pagination=new Pagination(totalPostCount,Long.parseLong(pageNo));
+			pagination=new Pagination(totalPostCount,Integer.parseInt(pageNo));
 		}		
-		request.setAttribute("list", BoardDAO.getInstance().findPostList(pagination));
+		request.setAttribute("list",dao.findPostList(pagination));
 		request.setAttribute("pagination", pagination);
 		request.setAttribute("url", "board/list.jsp");
-		return "layout.jsp";
-		
-		return "ProductPostMove.do";
+		request.setAttribute("url", "productboard/list.jsp");
+		return "mainpage.jsp";
 	}
 
 }
--- NongShim/src/main/java/model/ProductPostDAO.java
+++ NongShim/src/main/java/model/ProductPostDAO.java
@@ -116,4 +116,49 @@ public class ProductPostDAO {
 
 	}
 
+	public int getTotalPostCount() throws SQLException {
+		//데이터베이스의 NongShim_productPost 테이블의 총 개시물 수를 불러옴
+		//입력값: 없음
+		//출력값: 총 개시물 수 
+		ResultSet rs = null;
+		PreparedStatement pst = null;
+		Connection con = null;
+		int totalpostcount = 0;
+		try {
+			con = getConnection();
+			String sql = "SELECT COUNT(*) FROM NongShim_productPost";
+			pst = con.prepareStatement(sql);
+			rs = pst.executeQuery();
+			if(rs.next()) {
+				totalpostcount = rs.getInt(1);
+			}
+		} finally {
+			closeAll(rs, pst, con);
+		}
+		return totalpostcount;
+	}
+
+	public ArrayList<ProductPostVO> findPostList(Pagination pagination) throws SQLException {
+		//pagination의 시작값과 끝값을 바탕으로 NongShim_productPost에서 정해진 개수만큼의 게시글을 불러옴
+		//입력값: pagination
+		//출력값: 페이지 정보인 productpostvo 가 담긴 list를 반환
+		ArrayList<ProductPostVO> list = new ArrayList<>();
+		ResultSet rs = null;
+		PreparedStatement pst = null;
+		Connection con = null;
+		int totalpostcount = 0;
+		try {
+			con = getConnection();
+			String sql = "SELECT COUNT(*) FROM NongShim_productPost";
+			pst = con.prepareStatement(sql);
+			rs = pst.executeQuery();
+			if(rs.next()) {
+				totalpostcount = rs.getInt(1);
+			}
+		} finally {
+			closeAll(rs, pst, con);
+		}
+		return list;
+	}
+
 }
