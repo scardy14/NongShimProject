64196c13fbf41015af785fb687a23d4178d887d7
KOSTA <KOSTA@DESKTOP-T6561PQ>
2022-11-02 18:37:29 +0900

    테이블 이름 이걸로 다 맞춘다

--- NongShim/src/java/resources/NongShim-productPost.sql
+++ NongShim/src/java/resources/NongShim-productPost.sql
@@ -1,8 +1,9 @@
-drop sequence post_No_seq
+drop sequence postNo_seq
 drop table NongShim_product_Post
+drop table productpost1
 
 -- 2. Product Post 상품 게시판 글
-create table NongShim_productPost(
+create table NongShim_product_Post(
 post_No number primary key,
 title varchar2(100) not null,
 content clob not null,
@@ -25,18 +26,18 @@ CONSTRAINT fk_pp FOREIGN KEY(id) REFERENCES NongShim_Member(id)
 create sequence postNo_seq;
 
 -- 2.2 product post data 입력 
-insert into NongShim_productPost
+insert into NongShim_product_Post
 values (postNo_seq.nextval, '초록빛깔 상추', '신선하고 맛있습니다','java',default,'민석짱','코멘트부분',sysdate,'야채','status','상추',50,sysdate,5,10);
-insert into NongShim_productPost
+insert into NongShim_product_Post
 values (postNo_seq.nextval, '사과사과', '달콤하고 맛있습니다','spring',default,'민석짱2','코멘트부분',sysdate,'과일','status','사과',200,sysdate,10,30);
-insert into NongShim_productPost 
+insert into NongShim_product_Post 
 values (postNo_seq.nextval, '맛있는 햅쌀', '꼬들꼬들합니다','jdbc',default,'민석짱3','코멘트부분',sysdate,'곡물','status','쌀',150,sysdate,50,100);
 
 commit
 
 
 -- 2.3 product post 
-select * from NongShim_productpost
+select * from NongShim_product_post
 
 
 
--- NongShim/src/java/resources/drop-table.sql
+++ NongShim/src/java/resources/drop-table.sql
@@ -4,7 +4,7 @@
 drop table NongShim_Member
 
 -- 상품 게시판
-drop table NongShim_productPost
+drop table NongShim_product_Post
 
 -- 상품 게시판 댓글
 drop table NongShim_productPostComments
