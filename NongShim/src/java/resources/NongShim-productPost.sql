06fd76bd3d4209d8cabb4e0ae225b3da67fad97c
KOSTA <KOSTA@DESKTOP-T6561PQ>
2022-11-02 16:39:26 +0900

    민석 erd수정과 (상품info 상품 게시판과 통합_)!
    통합한 결과로 productPost 테이블 수정 완!

--- NongShim/src/java/resources/NongShim-productPost.sql
+++ NongShim/src/java/resources/NongShim-productPost.sql
@@ -1,5 +1,5 @@
 drop sequence postNo_seq
-
+drop table NongShim_productPost
 -- 2. Product Post 상품 게시판 글
 create table NongShim_productPost(
 postNo number primary key,
@@ -12,6 +12,12 @@ comments varchar2(100) not null,
 registerDate date not null,
 category varchar2(100) not null,
 status varchar2(100)not null,
+productName varchar2(100) not null,
+productPoint number not null,
+duration date not null,
+minCustomer number not null,
+maxCustomer number not null,
+
 CONSTRAINT fk_pp FOREIGN KEY(id) REFERENCES NongShim_Member(id)
 );
 
@@ -20,11 +26,13 @@ create sequence postNo_seq;
 
 -- 2.2 product post data 입력 
 insert into NongShim_productPost
-values (postNo_seq.nextval, '초록빛깔 상추', '신선하고 맛있습니다','java',default,'민석짱','코멘트부분',sysdate,'야채','status');
+values (postNo_seq.nextval, '초록빛깔 상추', '신선하고 맛있습니다','java',default,'민석짱','코멘트부분',sysdate,'야채','status','상추',50,sysdate,5,10);
 insert into NongShim_productPost
-values (postNo_seq.nextval, '사과사과', '달콤하고 맛있습니다','spring',default,'민석짱2','코멘트부분',sysdate,'과일','status');
+values (postNo_seq.nextval, '사과사과', '달콤하고 맛있습니다','spring',default,'민석짱2','코멘트부분',sysdate,'과일','status','사과',200,sysdate,10,30);
 insert into NongShim_productPost 
-values (postNo_seq.nextval, '맛있는 햅쌀', '꼬들꼬들합니다','jdbc',default,'민석짱3','코멘트부분',sysdate,'곡물','status');
+values (postNo_seq.nextval, '맛있는 햅쌀', '꼬들꼬들합니다','jdbc',default,'민석짱3','코멘트부분',sysdate,'곡물','status','쌀',150,sysdate,50,100);
+
+commit
 
 
 -- 2.3 product post 
