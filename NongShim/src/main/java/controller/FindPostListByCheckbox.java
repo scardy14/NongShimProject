9ccb1f9b14e0b53f696229658ac455f178956437
scardy14 <heperion14@gmail.com>
2022-11-03 11:51:02 +0900

    Pagination 홍주영

--- NongShim/src/main/java/controller/FindPostListByCheckbox.java
+++ NongShim/src/main/java/controller/FindPostListByCheckbox.java
@@ -1,18 +1,19 @@
 package controller;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import model.Pagination;
 import model.ProductPostDAO;
 
-public class FindPostListController implements Controller{
+public class FindPostListByCheckbox implements Controller {
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ProductPostDAO dao = ProductPostDAO.getInstance();
-		
+		String checkbox = request.getParameter("checkbox");
+		System.out.println(checkbox);
 		String pageNo=request.getParameter("pageNo");
 		Pagination pagination=null;
 		int totalPostCount=dao.getTotalPostCount();
@@ -21,8 +22,9 @@ public class FindPostListController implements Controller{
 		}else {
 			pagination=new Pagination(totalPostCount,Integer.parseInt(pageNo));
 		}		
-		request.setAttribute("list",dao.findPostList(pagination));
+		request.setAttribute("list",dao.findPostListByCheckbox(pagination, checkbox));
 		request.setAttribute("pagination", pagination);
+		request.setAttribute("url", "board/list.jsp");
 		request.setAttribute("url", "productboard/list.jsp");
 		return "mainpage.jsp";
 	}
