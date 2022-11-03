53de79e7f4cea6aa009e71ecfb8561d5d453286a
KOSTA <KOSTA@DESKTOP-T6561PQ>
2022-11-03 17:31:28 +0900

    그냥 풀받기위한 저장

--- NongShim/src/java/resources/minseok_test.sql
+++ NongShim/src/java/resources/minseok_test.sql
@@ -1,3 +1,8 @@
 select * from  NongShim_product_Post;
 
+select * from NongShim_productPostComments 
+
 select title,content, hits,nickname,comments,register_date,category,product_name,product_point,duration,min_customer,max_customer from NongShim_product_Post where post_no=2;
+
+
+insert into 
