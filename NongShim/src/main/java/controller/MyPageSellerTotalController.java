468a19dc027e3671fc6e18652159ab9f9ff9d372
989since <jdkb0918@gmail.com>
2022-11-07 00:14:14 +0900

    jdk-20221107-0014

--- NongShim/src/main/java/controller/MyPageSellerTotalController.java
+++ NongShim/src/main/java/controller/MyPageSellerTotalController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import model.MyPageDAO;
 import model.MyPageProductPostVO;
@@ -11,13 +13,17 @@ import model.MyPageProductPostVO;
 public class MyPageSellerTotalController implements Controller {
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String id="java";
+		//세션 받아오기
+		HttpSession session=request.getSession(false);
+		String id=session.;
+		System.out.println(id);
+		//리스트 받아오기
 		ArrayList<MyPageProductPostVO> list=MyPageDAO.getInstance().mySellProductListTotal(id);
-		String selling="판매중";
-		String sell="판매완료";
-		int sellingCount =MyPageDAO.getInstance().mySellProductListCount(selling, id);
-		int sellCount=MyPageDAO.getInstance().mySellProductListCount(sell, id);
-		System.out.println(list.toString());
+		//String selling="판매중";
+		//String sell="판매완료";
+		int sellingCount =MyPageDAO.getInstance().mySellProductListCount("판매중", id);
+		int sellCount=MyPageDAO.getInstance().mySellProductListCount("판매완료", id);
+		//System.out.println(list.toString());
 		request.setAttribute("sellerList",list);
 		request.setAttribute("url", "mypage/3seller.jsp");
 		request.setAttribute("selling", sellingCount);
--- NongShim/src/main/java/controller/MyPagecustomerController.java
+++ NongShim/src/main/java/controller/MyPagecustomerController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import member.NongShimMemberVO;
 import model.BuyProductVO;
 import model.MyPageDAO;
 import model.ProductPostDAO;
@@ -16,19 +17,22 @@ public class MyPagecustomerController implements Controller {
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		//세션-- > 연결해야함
-		//HttpSession session=request.getSession(false);
-		String id="java";
+		HttpSession session=request.getSession(false);
+		NongShimMemberVO mvo=(NongShimMemberVO) session.getAttribute("mvo");
+		String id=mvo.getId();
+		System.out.println("*"+id);
+		
 		//테스트
-		ArrayList<BuyProductVO> list=MyPageDAO.getInstance().myBuyProductListTotal(id);
-		String buying="구매중";
-		String buy="구매완료";
-		int buyingCount =MyPageDAO.getInstance().myBuyProductListCount(buying, id);
-		int buyCount=MyPageDAO.getInstance().myBuyProductListCount(buy, id);
-		request.setAttribute("list", list);
-		request.setAttribute("buyingCount", buyingCount);
-		request.setAttribute("buyCount", buyCount);
-		request.setAttribute("url","mypage/2customer.jsp");
-		return "mainpage.jsp";
+		 ArrayList<BuyProductVO> list=MyPageDAO.getInstance().myBuyProductListTotal(id); 
+		 String buying="구매중";
+		 String buy="구매완료"; 
+		 
+		 int buyingCount=MyPageDAO.getInstance().myBuyProductListCount(buying, id); 
+		 int buyCount=MyPageDAO.getInstance().myBuyProductListCount(buy, id);
+		 request.setAttribute("list", list); 
+		 request.setAttribute("buyingCount",buyingCount); request.setAttribute("buyCount", buyCount);
+		 request.setAttribute("url","mypage/2customer.jsp");
+		 return "mainpage.jsp";
 	}
 
 }
